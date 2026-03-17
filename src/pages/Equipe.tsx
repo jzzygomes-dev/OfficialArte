76742f84b0ac32b3886ee741f116f32f3f90186b
jzzygomes-dev <jzzygomes@gmail.com>
2026-03-17 14:20:28 +0100

    Update team images in Equipe component

--- src/pages/Equipe.tsx
+++ src/pages/Equipe.tsx
@@ -1,7 +1,7 @@
 import NewsNavbar from "@/components/NewsNavbar";
 import NewsFooter from "@/components/NewsFooter";
-import team1 from "@/assets/team-1.jpg";
-import team2 from "@/assets/team-2.jpg";
+import team1 from "@/assets/ceo.webp";
+import team2 from "@/assets/member1.webp";
 import team3 from "@/assets/team-3.jpg";
 import team4 from "@/assets/team-4.jpg";
 
